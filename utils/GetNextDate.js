5fdbfa09988cc297c2e26e8780ceeff751bfbfe0
vrushalisjoshi <vrushali.joshi99@gmail.com>
2021-05-11 00:03:32 +0530

    Aurangabad Vaccination Tracker Service date changes

--- utils/GetNextDate.js
+++ utils/GetNextDate.js
@@ -2,7 +2,7 @@ const getNextDate = () => {
   let today = (new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }).split(","))[0];
   today = today.split('/');
   var mm = parseInt(today[0]);
-  var dd = parseInt(today[1]);
+  var dd = parseInt(today[1])+1;
   var yyyy = parseInt(today[2]);
 
   return dd + "-" + mm + "-" + yyyy;
