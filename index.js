13b408d556d43dab5f0ec4bb445a829f6f456581
vrushalisjoshi <vrushali.joshi99@gmail.com>
2021-06-20 15:02:28 +0530

    --Not sending repeatative same messages

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ const bot = new TelegramBot(process.env.TOKEN, {
   polling: true,
 });
 
+let arrData = [];
+
 console.log(nextDate());
 
-let url = `${
-  process.env.COWIN_URL
-}appointment/sessions/public/calendarByDistrict?district_id=${process.env.ABD_ID}&date=`;
+let url = `${process.env.COWIN_URL}appointment/sessions/public/calendarByDistrict?district_id=${process.env.ABD_ID}&date=`;
 
 app.listen(PORT, () => {
   console.log(`server started on PORT ${PORT}`);
 });
 
-cron.schedule("*/5 * * * *", () => {
-  console.log("running a task every 5 minutes!");
-  console.log((new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })));
+cron.schedule("*/1 * * * *", () => {
+  console.log("running a task every 1 minute!");
+  console.log(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
   getVaccinationUpdates()();
 });
 
@@ -47,23 +47,33 @@ const getVaccinationUpdates = () => {
         return res.ok ? res.json() : res.text();
       })
       .then((json) => {
-        if(response) {
+        if (response) {
           response.send(json);
         }
         if (json.centers) {
           json.centers.forEach((centre) => {
             if (centre.sessions) {
               centre.sessions.forEach((session) => {
-                if(session.available_capacity && session.available_capacity > 0) {
-                  let message = `Vaccination available for age group ( ${session.min_age_limit}+ )
-                  \n on Date: ${session.date}
-                  \n Center Name: ${centre.name}
-                  \n PINCODE: ${centre.pincode}
-                  \n Vaccine: ${session.vaccine}
-                  \n Slots: ${session.slots}
-                  \n Dose1 Availability: ${session.available_capacity_dose1}
-                  \n Dose2 Availability: ${session.available_capacity_dose2}`;
-                  bot.sendMessage(process.env.telegram_chat_id, message);
+                if (!arrData[session.session_id])
+                  arrData[session.session_id] = 0;
+                if (
+                  session.available_capacity &&
+                  session.available_capacity > 0
+                ) {
+                  if (
+                    arrData[session.session_id] != session.available_capacity
+                  ) {
+                    arrData[session.session_id] = session.available_capacity;
+                    let message = `Vaccination available for age group ( ${session.min_age_limit}+ )
+                                    \n on Date: ${session.date}
+                                    \n Center Name: ${centre.name}
+                                    \n PINCODE: ${centre.pincode}
+                                    \n Vaccine: ${session.vaccine}
+                                    \n Slots: ${session.slots}
+                                    \n Dose1 Availability: ${session.available_capacity_dose1}
+                                    \n Dose2 Availability: ${session.available_capacity_dose2}`;
+                    bot.sendMessage(process.env.telegram_chat_id, message);
+                  }
                 }
               });
             }
@@ -75,7 +85,7 @@ const getVaccinationUpdates = () => {
         response.status(500);
         response.send(err);
       });
-  }
+  };
 };
 
 app.get("/", getVaccinationUpdates());
